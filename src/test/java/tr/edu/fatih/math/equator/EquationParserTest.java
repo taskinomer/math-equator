62f6b8b3e8c1833a85e4845eb6bf6de6e07dc4cc
Ömer Taşkın <otaskin@ebay.com>
2016-05-04 21:21:19 +0300

    ZeroStringException handled test was created

--- src/test/java/tr/edu/fatih/math/equator/EquationParserTest.java
+++ src/test/java/tr/edu/fatih/math/equator/EquationParserTest.java
@@ -17,17 +17,23 @@ public class EquationParserTest {
 
     @Test
     public void testEquator_withComplexEquation() throws ZeroStringException {
-        EquationParser equationParser = new EquationParser("(3+2*(1*(3+4+5)))");
+        EquationParser equationParser = new EquationParser("((3+5)*4)-3");
         String parsedString = equationParser.parse();
 
-        assertEquals(parsedString, "27");
+        assertEquals(parsedString, "29");
+    }
+
+    @Test
+    public void testEquator_withMoreComplexEquation() throws ZeroStringException {
+        EquationParser equationParser = new EquationParser("(3*(3+4)+3)*5");
+        String parsedString = equationParser.parse();
+
+        assertEquals(parsedString, "120");
     }
 
     @Test(expected = ZeroStringException.class)
     public void testEquator_withComplexEquation_shouldThrowZeroValueException() throws ZeroStringException {
         EquationParser equationParser = new EquationParser("(3+2*(1/(4-4)))");
-
-
-        System.out.println(equationParser.parse());
+        equationParser.parse();
     }
 }
